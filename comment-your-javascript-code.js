1fa9227dba0f6b8bd8be06b131f1ad5701a28918
karlosustic <98779471+karlosustic@users.noreply.github.com>
2022-02-09 22:04:40 +0100

    Update comment-your-javascript-code.js

--- comment-your-javascript-code.js
+++ comment-your-javascript-code.js
@@ -1,5 +1,5 @@
-Task 1 - You should create a // style comment that contains at least five letters.
-Task 2 - You should create a /* */ style comment that contains at least five letters.
+// Task 1 - You should create a // style comment that contains at least five letters.
+// Task 2 - You should create a /* */ style comment that contains at least five letters.
 
 
 //automobil
